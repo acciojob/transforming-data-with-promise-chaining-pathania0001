1e6a9ea8f2de163b67a2636ee3b6face3d3a36db
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-23 21:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+const input  =document.getElementById("ip")
+const button = document.getElementById("btn");
+ const wait = (delay)=>{
+	 return new Promise(resolve => {
+		 setTimeout(()=> resolve(),delay);
+	 });
+ }
+const promise = (value)=>{
+	return new Promise(async (resolve,reject)=>{
+		 await wait(2000);
+		output.innerText = `Result: ${value}`;
+		 resolve(value)
+	})
+}
+button.onclick = ()=>{
+  const {value} = input;
+  const prom = promise(value)
+	.then( async(data) => {  
+		await wait(2000) 
+		output.innerText = `Result: ${2*data}`;
+	  return 2*data;
+	}).then(async(data)=>{
+	  await wait(1000)
+		output.innerText = `Result: ${data-3}`;
+	  return data-3;
+	}).then(async(data)=>{
+      await wait(1000);
+		const val = (data/2);
+	    output.innerText = `Result: ${val}`;
+		return val;
+	}).then(async(data)=>{
+		await wait(1000);
+		const val = data + 10;
+		output.innerText = `Result :${val}`;
+		return val;
+	})
+}
+
+
